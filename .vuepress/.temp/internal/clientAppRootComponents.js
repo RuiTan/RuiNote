5229fab6bcd005b97939be2afc7995837d7d53f6
RuiTan <1063932362@qq.com>
2022-05-14 18:54:33 +0800

    初始化仓库

---
+++ .vuepress/.temp/internal/clientAppRootComponents.js
@@ -0,0 +1,9 @@
+import clientAppRootComponent0 from 'D:/Github/ruinote/node_modules/@mr-hope/vuepress-plugin-components/lib/client/root-components/BackToTop.js'
+import clientAppRootComponent1 from 'D:/Github/ruinote/node_modules/vuepress-plugin-pwa2/lib/client/global-components/SWUpdatePopup.js'
+import clientAppRootComponent2 from 'D:/Github/ruinote/node_modules/vuepress-plugin-pwa2/lib/client/global-components/PWAInstall.js'
+
+export const clientAppRootComponents = [
+  clientAppRootComponent0,
+  clientAppRootComponent1,
+  clientAppRootComponent2,
+]
